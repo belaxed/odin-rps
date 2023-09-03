2db54a0e9208d3ed75ffeffa6865bd793bf60243
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-02 20:05:55 -0400

    Add function to get computer choice.

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "Rock";
+    }
+    else if (randomNumber === 1) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
