f8e6f7a5903941e5f57dd4c199b1e9686bb152d6
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-04 09:45:59 -0400

    Add game of five

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let playerPointCounter = 0;
+let computerPointCounter = 0;
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const container = document.querySelector("#container");
+const outcomeDiv = document.querySelector(".outcome");
+const playerScoreSpan = document.querySelector(".player-score");
+const computerScoreSpan = document.querySelector(".computer-score");
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -14,66 +24,128 @@ function getComputerChoice() {
     }
 }
 
-let result = "";
-
-function playRockPaperScissors() {
-    computerSelection = getComputerChoice();
-
+function playRound(playerSelection, computerSelection) {
     if (playerSelection == "Rock") {
         if (computerSelection == "Rock") {
-            result = "Tie";
-            return "It's a tie! Rock ties Rock";
+            const p = document.createElement("p");
+            p.innerText = "It's a tie! Rock ties Rock";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection == "Paper") {
-            result = "Loss";
-            return "You Lose! Paper beats Rock";
+            computerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Lose! Paper beats Rock";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection = "Scissors") {
-            result = "Win";
-            return "You Win! Rock beats Scissors";
+            playerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Win! Rock beats Scissors";
+            outcomeDiv.appendChild(p);
         }
     }
     else if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
-            result = "Win";
-            return "You Win! Paper beats Rock";
+            playerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Win! Paper beats Rock";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection == "Paper") {
-            result = "Tie";
-            return "It's a tie! Paper ties Paper";
+            const p = document.createElement("p");
+            p.innerText = "It's a tie! Paper ties Paper";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection = "Scissors") {
-            result = "Loss";
-            return "You Lose! Scissors beats Paper";
+            computerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Lose! Scissors beats Paper";
+            outcomeDiv.appendChild(p);
         }
     }
     else if (playerSelection == "Scissors") {
         if (computerSelection == "Rock") {
-            result = "Loss";
-            return "You Lose! Rock beats Scissors";
+            computerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Lose! Rock beats Scissors";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection == "Paper") {
-            result = "Win";
-            return "You Win! Scissors beats Paper";
+            playerPointCounter++;
+            const p = document.createElement("p");
+            p.innerText = "You Win! Scissors beats Paper";
+            outcomeDiv.appendChild(p);
         }
         else if (computerSelection = "Scissors") {
-            result = "Tie";
-            return "It's a tie! Scissors ties Scissors";
+            const p = document.createElement("p");
+            p.innerText = "It's a tie! Scissors ties Scissors";
+            outcomeDiv.appendChild(p);
         }
     }
     else {
         result = "Error";
-        return "Error: Invalid move, please choose Rock, Paper, or Scissors.";
+        const p = document.createElement("p");
+            p.innerText = "Error: Invalid move, please choose Rock, Paper, or Scissors.";
+            outcomeDiv.appendChild(p);
     }
 }
 
+const checkForWinner = (playerPointCounter, computerPointCounter) => {
+    console.log("1 ", playerPointCounter, "2 ", computerPointCounter);
+    if (playerPointCounter === 5) {
+        const h2 = document.createElement("h2");
+        h2.classList.add("player-won");
+        h2.innerText = `You win the game ${playerPointCounter} to ${computerPointCounter}!`;
+        outcomeDiv.append(h2);
+    }
+    else if (computerPointCounter === 5) {
+        const h2 = document.createElement("h2");
+        h2.classList.add("computer-won");
+        h2.innerText = `You lost the game ${playerPointCounter} to ${computerPointCounter}...`;
+        outcomeDiv.append(h2);
+    }
+}
+
+const updateScores = (playerPointCounter, computerPointCounter) => {
+    playerScoreSpan.innerText = `You:${playerPointCounter}`;
+    computerScoreSpan.innerText = `Computer:${computerPointCounter}`;
+}
+
+rockButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "Rock";
+    playRound(playerSelection, computerSelection);
+    updateScores(playerPointCounter, computerPointCounter);
+    checkForWinner(playerPointCounter, computerPointCounter);
+})
+paperButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "Paper";
+    playRound(playerSelection, computerSelection);
+    updateScores(playerPointCounter, computerPointCounter);
+    checkForWinner(playerPointCounter, computerPointCounter);
+})
+scissorsButton.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "Scissors";
+    playRound(playerSelection, computerSelection);
+    updateScores(playerPointCounter, computerPointCounter);
+    checkForWinner(playerPointCounter, computerPointCounter);
+})
+/*
 function game() {
-    let playerPointCounter;
-    let computerPointCounter;
 
     while (playerPointCounter < 5 && computerPointCounter < 5) {
         
-        resultsDisplay.textContent = playRockPaperScissors();
+        buttons.forEach((button) => {
+            button.addEventListener("click", (e) => {
+                playerSelection = e.target.textContent;
+                //resultsDisplay.textContent = playRound();
+                console.log(playRound())
+            })
+            }
+        );
+        
 
         if (result == "Win") {
             playerPointCounter = playerPointCounter + 1;
@@ -82,7 +154,7 @@ function game() {
             computerPointCounter = computerPointCounter + 1;
         }
         
-        if (playerPointCounter == 5) {
+        else if (playerPointCounter == 5) {
             finalAnnouncement.textContent = "You Win the Game!";
         }
         else if (computerPointCounter == 5) {
@@ -90,23 +162,6 @@ function game() {
         }
         
         runningScoreDisplay.textContent = "Score: You: " + playerPointCounter + " Computer: " + computerPointCounter;
-        
     }
 }
-
-function getButtonClicked(e) {
-    playerSelection = e.target.textContent;
-    game();
-}
-
-const buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-    button.addEventListener("click", getButtonClicked)
-    }
-    );
-
-const resultsDisplay = document.querySelector("#resultsDisplay");
-
-const runningScoreDisplay = document.querySelector("#runningScoreDisplay");
-const finalAnnouncement = document.querySelector("#finalAnnouncement");
-
+*/
